128b2e1bee83cfdb6cee89cf696c09e1661dccb0
Abeer-Abdel <awabdelh@ncsu.edu>
2026-01-17 17:37:54 -0500

    Update SAR.py

--- SAR.py
+++ SAR.py
@@ -8,14 +8,13 @@ import pandas as pd
 
 # Config
 p = 0.5
-BASE_DIR = r"C:/Users/abora/Desktop/Research/4. Top-K list/Github_Submission"
-CSV_FILE = "Testt.csv"
+CSV  = "dataset.csv"
 ROW_FRACTION = 1.0
 SEED = 42
-os.chdir(BASE_DIR)
+
 
 # Load data
-df = pd.read_csv(CSV_FILE, header=None, dtype=str)
+df = pd.read_csv(CSV, header=None, dtype=str)
 orig_rows = df.shape[0]
 df = df.iloc[:max(1, int(orig_rows * ROW_FRACTION)), :]
 k = df.shape[0]   
@@ -263,3 +262,4 @@ swaps_time = end - time_before_swaps
 print(f"\nTime until swaps: {time_until_swaps:.3f} seconds")
 print(f"Time for swaps: {swaps_time:.3f} seconds")
 print(f"{dist0:.1f},{best_dist:.1f},{time_until_swaps:.3f},{swaps_time:.3f}")
+
